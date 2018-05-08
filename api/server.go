4294e4b5c785a3cadb77560e2e1abd9619c68c58
Nikolay Pustynnikov <pstnkv@gmail.com>
2018-05-08 18:44:09 +0300

    upd

--- api/server.go
+++ api/server.go
@@ -14,8 +14,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/robfig/cron"
 
-	"github.com/chainkorea/open-callisto-pool/storage"
-	"github.com/chainkorea/open-callisto-pool/util"
+	"github.com/pstnkv/open-callisto-pool/storage"
+	"github.com/pstnkv/open-callisto-pool/util"
 )
 
 type ApiConfig struct {
